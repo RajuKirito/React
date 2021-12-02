704e7ffaf43e89daf1df4716bed309ebadd58a76
Raju Ch <chakaragallaraju.1342001@gmail.com>
2021-12-03 00:42:56 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react"
+import './style.css'
+import Button from './Button'
+const App = () =>{
+    return(
+        <div>
+            <h1 style = {{textAlign:"center"}}>Hello World!</h1>)
+            {/* <button className = "button">App Store</button> 
+            <button className = 'button'>Play Store</button> */}
+            <Button title = 'App Store' />
+            <Button title = "Play Store" />
+            <Button />
+        </div>
+    );
+};
 
 export default App;
