a94bb734659c7e62ac94a46a819a06a188362e48
Raju Ch <chakaragallaraju.1342001@gmail.com>
2021-12-05 13:44:34 +0530

    pre final commit

---
+++ src/utils/config.js
@@ -0,0 +1,18 @@
+//TODO: add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyCwsyFVmSS8z6AKX8kX5L1dcnt2engokuI",
+  authDomain: "mygitapp-200be.firebaseapp.com",
+  databaseURL: "https://mygitapp-200be-default-rtdb.firebaseio.com",
+  projectId: "mygitapp-200be",
+  storageBucket: "mygitapp-200be.appspot.com",
+  messagingSenderId: "51524011178",
+  appId: "1:51524011178:web:08e5af5c44abdb6facbcd3",
+  measurementId: "G-XRGLT3877J"
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
